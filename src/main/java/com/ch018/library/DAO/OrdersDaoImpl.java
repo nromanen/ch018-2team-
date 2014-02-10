8a21b7ff71337ee86566b7fa778efe93a89671a0
etenzor <etenzor@gmail.com>
2014-02-10 18:41:43 +0200

    after merge

--- src/main/java/com/ch018/library/DAO/OrdersDaoImpl.java
+++ src/main/java/com/ch018/library/DAO/OrdersDaoImpl.java
@@ -209,10 +209,6 @@ public class OrdersDaoImpl implements OrdersDao {
 			// id)).list().get(0);
 	
 		}
-        @Override
-        public List<Orders> testCriteria(String title, String surname){
-            factory.getCurrentSession().createCriteria(Orders.class).add(Restrictions.eq())
-        }
 
 		@Override
 		public List<Orders> getOrdersBetweenDatesWithoutPerson(Person person, Book book, Date firstDate, Date secondDate) {
